61c4ee24842377f4d19565c6905f78c678362cca
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-02-29 16:09:01 +0000

    box lessons, boojings and reviews controllers created

---
+++ app/controllers/box_lessons_controller.rb
@@ -0,0 +1,39 @@
+class BoxLessonsController < ApplicationController
+  def index
+    @box_lessons = BoxLesson.all
+  end
+
+  def new
+    @box_lesson = BoxLesson.new(status: true)
+    authorize @box_lesson
+  end
+
+  def create
+    @box_lesson = BoxLesson.new(box_lesson_params)
+    authorize @box_lesson
+
+    if @box_lesson.save
+      redirect_to box_lessons_path
+    else
+      render :new
+    end
+    @booking = Booking.new
+    raise
+    @available_drop_ins = params[:capacity] - @box_lesson.booking.length
+  end
+
+  def edit
+    @box_lesson = BoxLesson.find(params[:id])
+  end
+
+  def update
+    @box_lesson = BoxLesson.find(params[:id])
+    @box_lesson.update(params[:box_lesson])
+  end
+
+  private
+
+  def box_lesson_params
+    params.require(:box_lesson).permit(:start_date_time, :end_date_time, :box_id, :capacity, :status, :price)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_145733) do
+ActiveRecord::Schema.define(version: 2020_02_29_154003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_145733) do
     t.string "student_phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "capacity"
     t.index ["box_lesson_id"], name: "index_bookings_on_box_lesson_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
