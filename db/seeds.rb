ac7c7260d3bd67d6298c5e534e4e0ea69ee2cb47
Mariana Mendes <mariana.omendes@gmail.com>
2020-02-29 17:08:24 +0000

    only user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+require 'open-uri'
+
+#file = open('https://res.cloudinary.com/woddi/image/upload/v1581782638/Showtime/woman-beard-transgender-lgbt-mtf-avatar-512_sdrnd1.png')
+
+# user.image.attach(io: file, filename: 'user-image.jpg')
+
+# url = 'https://res.cloudinary.com/woddi/image/upload/v1581782638/Showtime/woman-beard-transgender-lgbt-mtf-avatar-512_sdrnd1.png'
+
+file = URI.open('https://res.cloudinary.com/woddi/image/upload/v1581782638/Showtime/woman-beard-transgender-lgbt-mtf-avatar-512_sdrnd1.png')
+# filename = File.basename(URI.parse(url).path)
+
+User.destroy_all
+Box.destroy_all
+BoxLesson.destroy_all
+Booking.destroy_all
+
+booking_status = ["active", "cancelled", "redeemed"]
+
+puts 'Creating 10 random users...'
+
+10.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    dob: Date.today-25.years,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.save!
+end
+
+user_test = User.create(first_name: "Maria", last_name: "Silva", dob: "21/02/1980", email: "lewagon@gmail.com", password: "123456")
+
+
