ddb3174310b22f5321c3cb25a40afefb2b6aec32
nuno tobias <nmtobias@gmail.com>
2020-03-05 20:57:01 +0000

    added maps and markers

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,5 +1,5 @@
 class BoxesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :map]
 
   def index
     if params[:query].present?
@@ -7,7 +7,29 @@ class BoxesController < ApplicationController
     else
       @boxes = policy_scope(Box).all
     end
+
+    @boxes = Box.geocoded #returns flats with coordinates
+
+    @markers = @boxes.map do |box|
+      {
+        lat: box.latitude,
+        lng: box.longitude
+      }
+    end
   end
+
+  def map
+    @boxes = policy_scope(Box).all
+    authorize @boxes
+    @boxes = Box.geocoded #returns flats with coordinates
+
+    @markers = @boxes.map do |box|
+      {
+        lat: box.latitude,
+        lng: box.longitude
+      }
+    end
+  end 
 
   def show
     @booking = Booking.new
