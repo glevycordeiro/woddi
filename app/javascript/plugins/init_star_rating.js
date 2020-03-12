6b3fbf499cbb9839d589bc033d76ea4348f14f29
Goncalo Cordeiro <glevycordeiro@gmail.com>
2020-03-12 20:19:48 +0000

    finished and corrected boxes page

---
+++ app/javascript/plugins/init_star_rating.js
@@ -0,0 +1,13 @@
+import "jquery-bar-rating";
+import "jquery-bar-rating/dist/themes/css-stars.css";
+import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuery section)
+
+const initStarRating = () => {
+  $('#review_rating').barrating({
+    theme: 'css-stars',
+    initialRating: 1,
+    deselectable: false
+  });
+};
+
+export { initStarRating };
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   end
   get 'map', to: 'boxes#map', as: :map
 
-  resources :bookings, only: [:index]
-
+  resources :bookings, only: [:index] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
