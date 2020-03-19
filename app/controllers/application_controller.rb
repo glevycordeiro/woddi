71c7198468f62a493aefaecf27c4c9b230a7a4cd
Frederic Baugnee <fredericbaugnee@gmail.com>
2020-03-19 22:00:45 +0000

    redirect completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :store_user_location!, if: :storable_location?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  after_action :store_action
 
   include Pundit
 
@@ -10,6 +11,30 @@ class ApplicationController < ActionController::Base
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
 
+def after_sign_in_path_for(resource_or_scope)
+  stored_location_for(resource_or_scope) || super
+end
+
+def after_sign_up_path_for(resource_or_scope)
+  stored_location_for(resource_or_scope) || super
+end
+
+def after_edit_path_for(resource_or_scope)
+  stored_location_for(resource_or_scope) || super
+end
+
+  def store_action
+    return unless request.get?
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
+  end
 
   def configure_permitted_parameters
   # For additional fields in app/views/devise/registrations/new.html.erb
@@ -37,6 +62,15 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { host: "www.woddi.club" || "localhost:3000" }
+  end
+
+  def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+      # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
   end
 
 
